131b18a7cc5da558cdf1e45693a92bd75c84a1c5
NikolayM <nm@nomadcy.com>
2023-02-19 19:14:26 +0200

    production ready

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,6 @@
 export default {
 	port: '8000',
 	origin: 'http://127.0.0.1:5173',
-	dbUri: 'postgres://nikolaym:12351235@localhost:5432/ghapi',
 	emailUser: 'n.mihaylovv@gmail.com',
 	pass: 'trnvizxespftfbbl'
 }
