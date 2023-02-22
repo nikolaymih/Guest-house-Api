4e85539427203598d989388ff4167ad66d0de7e3
nikolaymih <nikolaymih@abv.bg>
2023-02-22 22:51:23 +0200

    remove info

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,5 @@
 export default {
-	devPort: '8000',
-	prodPort: '443',
+	port: '8000',
 	origin: 'http://127.0.0.1:5173',
 	emailUser: 'n.mihaylovv@gmail.com',
 	pass: 'trnvizxespftfbbl'
--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import routes from "./routes";
 
 const app: Express = express();
 
-const port = config.get<string>(process.env.NODE_ENV === 'development' ? 'devPort' : 'prodPort');
+const port = config.get<string>('port');
 const origin = config.get<string>('origin');
 
 app.use(cors({
