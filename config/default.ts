db0a4ddedc59b5be0180c696433f3ddc05b800ab
nikolaymih <nikolaymih@abv.bg>
2023-04-01 11:50:40 +0300

    remove default config

--- config/default.ts
+++
@@ -1,7 +0,0 @@
-export default {
-	DATABASE_URL: "postgresql://postgres:ApsL8QWpVm8NczwMfOuh@containers-us-west-174.railway.app:6836/railway",
-	port: '8000',
-	origin: 'http://127.0.0.1:5173',
-	emailUser: 'n.mihaylovv@gmail.com',
-	pass: 'trnvizxespftfbbl'
-}
