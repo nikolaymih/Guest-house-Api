bee9322178ac6c3e3334b5fb44484ccbe05ad80c
nikolaymih <nikolaymih@abv.bg>
2023-04-05 01:12:47 +0300

    change name again for user

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,14 +1,15 @@
 import nodeMailer from 'nodemailer';
+import config from "config";
 import {createReservationType} from "../schema/reservation.schema";
 
-const receiver = process.env.EMAIL_USER;
+const user = process.env.EMAIL_USER;
 const pass = process.env.PASS;
 
 export const transporter = nodeMailer.createTransport({
 	port: 465,
 	host: 'smtp.gmail.com',
 	auth: {
-		user: receiver,
+		user,
 		pass
 	},
 	secure: true
@@ -16,8 +17,8 @@ export const transporter = nodeMailer.createTransport({
 
 export const configureMailMessage = ({fullName, email, phone, subject, message}: createReservationType['body']) => {
 	return {
-		from: email,  // sender address
-		to: receiver,   // list of receivers
+		from: user,  // sender address
+		to: user,   // list of receivers
 		subject: 'Ново запитване за резервация',
 		text: 'That was easy!',
 		html: `<b>Туко що бе получено ново запитване за резервация със съответната информация:</b><br/>
